14ab0821440ef6ae351a68e01eb1161107c0b19f
Srikar Narra <srikarnarra16@gmail.com>
2026-07-04 11:30:23 +0530

    Auto-save: 2026-07-04 11:30:23

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Lenis from "@studio-freight/lenis";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
