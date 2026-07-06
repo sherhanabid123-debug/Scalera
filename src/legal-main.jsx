24ef2a6ee908e7a4b2fbfa9e86a64797f796897d
Srikar Narra <srikarnarra16@gmail.com>
2026-07-06 18:49:45 +0530

    Auto-save: 2026-07-06 18:49:45

---
+++ src/legal-main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Legal from "./components/legal/Legal.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Legal />
+  </React.StrictMode>,
+);
