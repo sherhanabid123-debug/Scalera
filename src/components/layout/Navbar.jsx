af154113183ad184203b364f13361fac76f62e54
Srikar Narra <srikarnarra16@gmail.com>
2026-07-06 17:46:39 +0530

    Auto-save: 2026-07-06 17:46:39

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -410,6 +410,7 @@ const Navbar = ({ loading }) => {
                 letterSpacing: "-0.03em",
                 textTransform: "uppercase",
                 fontFamily: "var(--font-display)",
+                color: "#ffffff",
               }}
             >
               {link.label}
